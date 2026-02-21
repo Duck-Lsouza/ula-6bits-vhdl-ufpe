c8c47d1eb09e90176bd8b3c02c25be80fb6ebdca
Lúcio Vagner <159669475+Duck-Lsouza@users.noreply.github.com>
2026-02-20 21:39:09 -0300

    Feat: full_adder
    Terminei o arquivo do somador completo e o validei por simulação

--- src/components/full_adder.vhd
+++ src/components/full_adder.vhd
@@ -1,1 +1,22 @@
-fulladder
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity full_adder is
+	port(
+		a : in std_logic;
+		b : in std_logic;
+		cin : in std_logic;
+		s : out std_logic;
+		cout : out std_logic 
+	
+	);
+	end entity;
+	
+	architecture arch_full_adder of full_adder is 
+	begin
+		-- a equação da soma é s =
+		
+		s <= a xor b xor cin;
+		
+		cout <= (a and b) or (a and cin) or (b and cin);
+end architecture;
