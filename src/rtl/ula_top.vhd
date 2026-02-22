b9cb16eb3d0603ba8913df72889b7acf21a22ddb
Lúcio Vagner <159669475+Duck-Lsouza@users.noreply.github.com>
2026-02-22 20:41:06 -0300

    Arquivo top level testado por simulação

--- src/rtl/ula_top.vhd
+++ src/rtl/ula_top.vhd
@@ -31,263 +31,174 @@ entity ula_top is
         hex_s_dez   : out std_logic_vector(6 downto 0);
         hex_s_uni   : out std_logic_vector(6 downto 0)
     );
-end entity;
+end ula_top;
 
 architecture structural of ula_top is
-
   
-    -- tamo declarando os componentes tamo declarando
- 
-
     component arithmetic_unit is
-        port(
-            a, b : in  std_logic_vector(5 downto 0);
-            ctrl : in  std_logic;
-            s    : out std_logic_vector(5 downto 0);
-            cout : out std_logic;
+        port (
+            a, b     : in std_logic_vector(5 downto 0);
+            ctrl     : in std_logic; 
+            s        : out std_logic_vector(5 downto 0);
+            cout     : out std_logic;
             overflow : out std_logic
         );
     end component;
 
     component logic_unit is
         port(
-            a, b : in  std_logic_vector(5 downto 0);
-            sel  : in  std_logic_vector(1 downto 0);
+            a, b : in std_logic_vector(5 downto 0);
+            sel  : in std_logic_vector(1 downto 0); 
             s    : out std_logic_vector(5 downto 0)
         );
     end component;
 
     component shifter_unit is
         port(
-            a, b : in  std_logic_vector(5 downto 0);
-            sel  : in  std_logic_vector(1 downto 0);
+            a, b : in std_logic_vector(5 downto 0);
+            sel  : in std_logic_vector (1 downto 0); 
             s    : out std_logic_vector(5 downto 0)
-        );
-    end component;
-
-    component comparators_unit is
-        port(
-            a, b : in  std_logic_vector(5 downto 0);
-            s    : in  std_logic_vector(5 downto 0);
-            zero : out std_logic;
-            neg  : out std_logic;
-            ig   : out std_logic;
-            me   : out std_logic
         );
     end component;
 
     component mux_16to1 is
         port(
             sel      : in  std_logic_vector(3 downto 0);
-            a_add_b  : in  std_logic_vector(5 downto 0);
-            a_sub_b  : in  std_logic_vector(5 downto 0);
-            a_or_b   : in  std_logic_vector(5 downto 0);
-            a_and_b  : in  std_logic_vector(5 downto 0);
-            not_a    : in  std_logic_vector(5 downto 0);
-            not_b    : in  std_logic_vector(5 downto 0);
-            sl_a     : in  std_logic_vector(5 downto 0);
-            sr_a     : in  std_logic_vector(5 downto 0);
-            pass_a   : in  std_logic_vector(5 downto 0);
-            pass_b   : in  std_logic_vector(5 downto 0);
-            sl_b     : in  std_logic_vector(5 downto 0);
-            sr_b     : in  std_logic_vector(5 downto 0);
-            a_inc    : in  std_logic_vector(5 downto 0);
-            a_dec    : in  std_logic_vector(5 downto 0);
+            a_add_b  : in  std_logic_vector(5 downto 0); 
+            a_sub_b  : in  std_logic_vector(5 downto 0); 
+            a_or_b   : in  std_logic_vector(5 downto 0); 
+            a_and_b  : in  std_logic_vector(5 downto 0); 
+            not_a    : in  std_logic_vector(5 downto 0); 
+            not_b    : in  std_logic_vector(5 downto 0); 
+            sl_a     : in  std_logic_vector(5 downto 0); 
+            sr_a     : in  std_logic_vector(5 downto 0); 
+            pass_a   : in  std_logic_vector(5 downto 0); 
+            pass_b   : in  std_logic_vector(5 downto 0); 
+            sl_b     : in  std_logic_vector(5 downto 0); 
+            sr_b     : in  std_logic_vector(5 downto 0); 
+            a_inc    : in  std_logic_vector(5 downto 0); 
+            a_dec    : in  std_logic_vector(5 downto 0); 
             s        : out std_logic_vector(5 downto 0)
+        );
+    end component;
+
+    component comparators_unit is
+        port(
+            a, b : in std_logic_vector (5 downto 0);
+            s    : in std_logic_vector(5 downto 0);
+            zero : out std_logic;
+            neg  : out std_logic;
+            ig   : out std_logic; 
+            me   : out std_logic 
         );
     end component;
 
     component seg7_ab is
         port(
-            entrada  : in  std_logic_vector(5 downto 0);
-            sinal    : out std_logic;
-            saida0   : out std_logic_vector(6 downto 0);
-            saida1   : out std_logic_vector(6 downto 0)
+            entrada : in  std_logic_vector(5 downto 0);
+            sinal   : out std_logic;                   
+            saida0  : out std_logic_vector(6 downto 0); 
+            saida1  : out std_logic_vector(6 downto 0)  
         );
     end component;
 
     component seg7_s is
         port(
-            entrada  : in  std_logic_vector(5 downto 0);
-            saida1   : out std_logic_vector(6 downto 0);
-            saida2   : out std_logic_vector(6 downto 0);
-            saida3   : out std_logic_vector(6 downto 0)
+            entrada : in  std_logic_vector(5 downto 0);
+            saida1  : out std_logic_vector(6 downto 0); 
+            saida2  : out std_logic_vector(6 downto 0); 
+            saida3  : out std_logic_vector(6 downto 0)  
         );
     end component;
 
    
-    signal wire_add    : std_logic_vector(5 downto 0); -- A + B
-    signal wire_sub    : std_logic_vector(5 downto 0); -- A - B
-    signal wire_inc    : std_logic_vector(5 downto 0); -- A + 1
-    signal wire_dec    : std_logic_vector(5 downto 0); -- A - 1
-    signal wire_logic  : std_logic_vector(5 downto 0); -- saida logica
-    signal wire_shift  : std_logic_vector(5 downto 0); -- saida shift
+    signal wire_b_arith : std_logic_vector(5 downto 0);
+    signal ctrl_arith   : std_logic;
+    signal sel_logic    : std_logic_vector(1 downto 0);
+    signal sel_shift    : std_logic_vector(1 downto 0);
 
-    -- Saidas logica para o mux
-    signal wire_or_ab  : std_logic_vector(5 downto 0);
-    signal wire_and_ab : std_logic_vector(5 downto 0);
-    signal wire_not_a  : std_logic_vector(5 downto 0);
-    signal wire_not_b  : std_logic_vector(5 downto 0);
-
-    -- Saidas shift separadas (para o mux)
-    signal wire_sl_a   : std_logic_vector(5 downto 0);
-    signal wire_sr_a   : std_logic_vector(5 downto 0);
-    signal wire_sl_b   : std_logic_vector(5 downto 0);
-    signal wire_sr_b   : std_logic_vector(5 downto 0);
-
-    -- Saida final da ULA (saida do mux)
-    signal wire_s      : std_logic_vector(5 downto 0);
-
-    -- Overflows e couts
-    signal wire_cout_add  : std_logic;
-    signal wire_cout_sub  : std_logic;
-    signal wire_cout_inc  : std_logic;
-    signal wire_cout_dec  : std_logic;
-    signal wire_ov_add    : std_logic;
-    signal wire_ov_sub    : std_logic;
-    signal wire_ov_inc    : std_logic;
-    signal wire_ov_dec    : std_logic;
+    signal wire_s_arith : std_logic_vector(5 downto 0);
+    signal wire_s_logic : std_logic_vector(5 downto 0);
+    signal wire_s_shift : std_logic_vector(5 downto 0);
+    
+    -- Fio do MUX para a saída
+    signal wire_s_final : std_logic_vector(5 downto 0);
+    signal wire_ov_arith: std_logic;
 
 begin
 
-    U_ADD: arithmetic_unit port map(
+
+    wire_b_arith <= "000001" when (pin_sel = "1100" or pin_sel = "1101") else pin_B;
+    
+    -- Se for Subtração(0001) ou Decremento(1101), ativa o complemento de 2.
+    ctrl_arith <= '1' when (pin_sel = "0001" or pin_sel = "1101") else '0';
+
+						--  Unidade Lógica: 00=OR(0010), 01=AND(0011), 10=NOT_A(0100), 11=NOT_B(0101)
+    sel_logic <= "00" when pin_sel = "0010" else
+                 "01" when pin_sel = "0011" else
+                 "10" when pin_sel = "0100" else
+                 "11" when pin_sel = "0101" else
+                 "00";
+
+   --Unidade de Deslocamento: 00=SLA(0110), 01=SRA(0111), 10=SLB(1010), 11=SRB(1011)
+    sel_shift <= "00" when pin_sel = "0110" else
+                 "01" when pin_sel = "0111" else
+                 "10" when pin_sel = "1010" else
+                 "11" when pin_sel = "1011" else
+                 "00";
+					  
+    U_ARITH: arithmetic_unit port map(
         a        => pin_A,
-        b        => pin_B,
-        ctrl     => '0',
-        s        => wire_add,
-        cout     => wire_cout_add,
-        overflow => wire_ov_add
+        b        => wire_b_arith,
+        ctrl     => ctrl_arith,
+        s        => wire_s_arith,
+        cout     => open, 
+        overflow => wire_ov_arith
     );
 
-   
-    U_SUB: arithmetic_unit port map(
-        a        => pin_A,
-        b        => pin_B,
-        ctrl     => '1',
-        s        => wire_sub,
-        cout     => wire_cout_sub,
-        overflow => wire_ov_sub
-    );
-
- 
-    U_INC: arithmetic_unit port map(
-        a        => pin_A,
-        b        => "000001",
-        ctrl     => '0',
-        s        => wire_inc,
-        cout     => wire_cout_inc,
-        overflow => wire_ov_inc
-    );
-
-   
-    U_DEC: arithmetic_unit port map(
-        a        => pin_A,
-        b        => "000001",
-        ctrl     => '1',
-        s        => wire_dec,
-        cout     => wire_cout_dec,
-        overflow => wire_ov_dec
-    );
-
- 
-    -- Unidade Logica: OR (sel=00), AND (sel=01), NOT A (sel=10), NOT B (sel=11)
-    -- Instanciada 4 vezes com sel fixo para obter cada resultado separado
-    -
-    U_OR: logic_unit port map(
+    U_LOGIC: logic_unit port map(
         a   => pin_A,
         b   => pin_B,
-        sel => "00",
-        s   => wire_or_ab
+        sel => sel_logic,
+        s   => wire_s_logic
     );
 
-    U_AND: logic_unit port map(
+    U_SHIFT: shifter_unit port map(
         a   => pin_A,
         b   => pin_B,
-        sel => "01",
-        s   => wire_and_ab
+        sel => sel_shift,
+        s   => wire_s_shift
     );
-
-    U_NOTA: logic_unit port map(
-        a   => pin_A,
-        b   => pin_B,
-        sel => "10",
-        s   => wire_not_a
-    );
-
-    U_NOTB: logic_unit port map(
-        a   => pin_A,
-        b   => pin_B,
-        sel => "11",
-        s   => wire_not_b
-    );
-
- 
-    U_SLA: shifter_unit port map(
-        a   => pin_A,
-        b   => pin_B,
-        sel => "00",
-        s   => wire_sl_a
-    );
-
-    U_SRA: shifter_unit port map(
-        a   => pin_A,
-        b   => pin_B,
-        sel => "01",
-        s   => wire_sr_a
-    );
-
-    U_SLB: shifter_unit port map(
-        a   => pin_A,
-        b   => pin_B,
-        sel => "10",
-        s   => wire_sl_b
-    );
-
-    U_SRB: shifter_unit port map(
-        a   => pin_A,
-        b   => pin_B,
-        sel => "11",
-        s   => wire_sr_b
-    );
-
     U_MUX: mux_16to1 port map(
         sel     => pin_sel,
-        a_add_b => wire_add,
-        a_sub_b => wire_sub,
-        a_or_b  => wire_or_ab,
-        a_and_b => wire_and_ab,
-        not_a   => wire_not_a,
-        not_b   => wire_not_b,
-        sl_a    => wire_sl_a,
-        sr_a    => wire_sr_a,
+        a_add_b => wire_s_arith, 
+        a_sub_b => wire_s_arith, 
+        a_or_b  => wire_s_logic,
+        a_and_b => wire_s_logic,
+        not_a   => wire_s_logic,
+        not_b   => wire_s_logic,
+        sl_a    => wire_s_shift,
+        sr_a    => wire_s_shift,
         pass_a  => pin_A,
         pass_b  => pin_B,
-        sl_b    => wire_sl_b,
-        sr_b    => wire_sr_b,
-        a_inc   => wire_inc,
-        a_dec   => wire_dec,
-        s       => wire_s
+        sl_b    => wire_s_shift,
+        sr_b    => wire_s_shift,
+        a_inc   => wire_s_arith, 
+        a_dec   => wire_s_arith, 
+        s       => wire_s_final
     );
-
-    -- Saida principal da ULA
-    pin_S <= wire_s;
-
-   
-    flag_overflow <= wire_ov_add when pin_sel = "0000" else
-                     wire_ov_sub when pin_sel = "0001" else
-                     wire_ov_inc when pin_sel = "1100" else
-                     wire_ov_dec when pin_sel = "1101" else
-                     '0';
 
     U_CMP: comparators_unit port map(
         a    => pin_A,
         b    => pin_B,
-        s    => wire_s,
+        s    => wire_s_final,
         zero => flag_zero,
         neg  => flag_negativo,
         ig   => flag_igualdade,
         me   => flag_grandeza
     );
+
+
     U_DISP_A: seg7_ab port map(
         entrada => pin_A,
         sinal   => led_sinal_a,
@@ -303,10 +214,17 @@ begin
     );
 
     U_DISP_S: seg7_s port map(
-        entrada => wire_s,
+        entrada => wire_s_final,
         saida1  => hex_s_sin,
         saida2  => hex_s_dez,
         saida3  => hex_s_uni
     );
 
-end architecture;
+    
+    -- oia as saidas finais ai 
+    pin_S <= wire_s_final;
+    
+    -- O Overflow só vai ser valido nas operacoes aritmeticaas
+    flag_overflow <= wire_ov_arith when (pin_sel = "0000" or pin_sel = "0001" or pin_sel = "1100" or pin_sel = "1101") else '0';
+
+end structural;
